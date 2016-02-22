c36384568a520df9c3fc6780d08d181096c8cd84
Leandro Zanuz <lzanuz@gmail.com>
2016-02-22 12:03:07 -0300

    Adjustments urls.

--- ajaximage/urls.py
+++ ajaximage/urls.py
@@ -5,7 +5,7 @@ from django.conf.urls import url
 from ajaximage import views
 
 
-patterns = [
+urlpatterns = [
 
     url(
         '^upload/(?P<upload_to>.*)/(?P<max_width>\d+)/(?P<max_height>\d+)/(?P<crop>\d+)',
